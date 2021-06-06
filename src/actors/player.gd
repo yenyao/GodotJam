d0fa4d55f40316c627e63641fa66f9b75e813fd3
[Justin Soh] <ys00715@surrey.ac.uk>
2021-06-06 01:44:13 +0100

    Add player rotation

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -8,7 +8,12 @@ var direction = Vector2(0,0)
 func _physics_process(delta):
 	velocity = move_player()
 
+func _process(delta):
+	# Player faces mouse cursor
+	look_at(get_global_mouse_position())
 
+
+# Moves player in 4 directions
 func move_player():
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
