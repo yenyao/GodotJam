f39d5d9decd4393b1bf6390a384899fe9f0427d6
Jed Thompson <jedster1111@hotmail.co.uk>
2021-06-07 23:16:51 +0100

    Normalize player direction
    Avoids player moving faster diagonally

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -17,5 +17,5 @@ func _process(delta):
 func move_player():
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	velocity = speed * get_physics_process_delta_time() * direction
+	velocity = speed * get_physics_process_delta_time() * direction.normalized()
 	return move_and_slide(velocity)
