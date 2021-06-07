86b9c59275d6e74736df961de7f464b6f1c79ee9
[Justin Soh] <ys00715@surrey.ac.uk>
2021-06-08 00:19:16 +0100

    Bouncy rotating bullets

--- src/actors/Bullet.gd
+++ src/actors/Bullet.gd
@@ -1,10 +1,23 @@
 extends KinematicBody2D
 
-var velocity = Vector2(100,0)
+var velocity = Vector2(500,-100)
+
+func _ready():
+	rotation =  get_angle(velocity)
 
 func _physics_process(_delta):
 	move_bullet()
+	
 
 func move_bullet():
-	velocity = move_and_slide(velocity)
+	var collision = move_and_collide(velocity * get_physics_process_delta_time())
+	#print(collision)
 
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+		rotation =  get_angle(velocity)
+
+	return collision
+
+func get_angle(velocity):
+	return velocity.angle() + PI/2
