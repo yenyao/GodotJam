d5d22387b6efb7a61af7da1bf57a063b010f50ca
Mitchell <mitchell.sharman@hotmail.co.uk>
2021-06-10 23:36:50 +0100

    grouped enemies for proper movement

--- src/Levels/FirstScene.gd
+++ src/Levels/FirstScene.gd
@@ -4,7 +4,8 @@ func _on_Player_shoot(bullet):
 	bullet.velocity *= 0.5
 	add_child(bullet)
 func _ready():
-	var Enemy = get_node("Enemy") 
+	var Enemies = get_tree().get_nodes_in_group("Enemies")
 	var Player = get_node("Player")
-	Enemy.target = Player
+	for Enemy in Enemies:
+		Enemy.target = Player
 
